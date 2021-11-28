9782f700ce79a14361dd6d93602eed75bb1d27cf
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2021-11-28 16:44:31 +0100

    Change selected border color

--- config.h
+++ config.h
@@ -15,7 +15,7 @@ static const char col_cyan[]        = "#50a14f";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,   "#ca1243" },
 };
 
 /* tagging */
