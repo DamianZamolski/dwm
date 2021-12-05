396b7cb969003e69b73002f3233e513b0b3dd8e7
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2021-12-05 11:38:10 +0100

    Change dmenu colors

--- config.h
+++ config.h
@@ -56,13 +56,15 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", green, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *webBrowserCmd[]  = { "firefox-developer-edition", NULL };
+static const char *passCmd[]  = { "passmenu", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = passCmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = webBrowserCmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
