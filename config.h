69d4a949298c02fc3252558a03895fa08ae954ae
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2021-12-28 10:13:00 +0100

    Add screenshot mappings

--- config.h
+++ config.h
@@ -31,15 +31,19 @@ static const char *stCmd[] = {"st", NULL};
 static const char *firefoxCmd[] = {"firefox-developer-edition", NULL};
 static const char *passMenuCmd[] = {"passmenu", NULL};
 static const char *screenshotCmd[] = {"screenshot", NULL};
+static const char *screenshotToClipboardCmd[] = {"screenshot-to-clipboard", NULL};
 static const char *selectionScreenshotCmd[] = {"selection-screenshot", NULL};
+static const char *selectionScreenshotToClipboardCmd[] = {"selection-screenshot-to-clipboard", NULL};
 
 static const Key keys[] = {
   {MODKEY, XK_Return, spawn, {.v = stCmd}},
   {MODKEY, XK_p, spawn, {.v = dmenuCmd}},
   {MODKEY, XK_f, spawn, {.v = firefoxCmd}},
   {MODKEY|ShiftMask, XK_p, spawn, {.v = passMenuCmd}},
-  {MODKEY, XK_s, spawn, {.v = screenshotCmd}},
-  {MODKEY|ShiftMask, XK_s, spawn, {.v = selectionScreenshotCmd}},
+  {MODKEY, XK_Print, spawn, {.v = screenshotCmd}},
+  {MODKEY|ControlMask, XK_Print, spawn, {.v = screenshotToClipboardCmd}},
+  {MODKEY|ShiftMask, XK_Print, spawn, {.v = selectionScreenshotCmd}},
+  {MODKEY|ControlMask|ShiftMask, XK_Print, spawn, {.v = selectionScreenshotToClipboardCmd}},
   {MODKEY, XK_b, togglebar, {0}},
   {MODKEY, XK_j, focusstack, {.i = +1}},
   {MODKEY, XK_k, focusstack, {.i = -1}},
