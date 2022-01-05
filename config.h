a7f8b606e92878554eff326371b0f6b82113745d
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-01-05 08:33:47 +0100

    Add mappings

--- config.h
+++ config.h
@@ -29,37 +29,47 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0";
 
 static const char *dmenuCmd[] = {"dmenu_run", NULL};
-static const char *stCmd[] = {"st", NULL};
 static const char *firefoxCmd[] = {"firefox-developer-edition", NULL};
-static const char *passMenuCmd[] = {"passmenu", NULL};
+static const char *rocketchatCmd[] = {"rocketchat-desktop", NULL};
+static const char *stCmd[] = {"st", NULL};
+static const char *thunderbirdCmd[] = {"thunderbird", NULL};
+
 static const char *screenshotCmd[] = {"screenshot", NULL};
 static const char *screenshotToClipboardCmd[] = {"screenshot-to-clipboard", NULL};
 static const char *selectionScreenshotCmd[] = {"selection-screenshot", NULL};
 static const char *selectionScreenshotToClipboardCmd[] = {"selection-screenshot-to-clipboard", NULL};
+
 static const char *increaseVolumeCmd[] = {"amixer", "-c", "1", "sset", "Master", "1%+", NULL};
 static const char *decreaseVolumeCmd[] = {"amixer", "-c", "1", "sset", "Master", "1%-", NULL};
 static const char *setMaxVolumeCmd[] = {"amixer", "-c", "1", "sset", "Master", "100%", NULL};
 static const char *muteVolumeCmd[] = {"amixer", "-c", "1", "sset", "Master", "0%", NULL};
+
 static const char *increaseBrightnessCmd[] = {"xbacklight", "-inc", "1", NULL};
 static const char *decreaseBrightnessCmd[] = {"xbacklight", "-dec", "1", NULL};
 static const char *setMaxBrightnessCmd[] = {"xbacklight", "-set", "100", NULL};
 
 static const Key keys[] = {
   {MODKEY, XK_Return, spawn, {.v = stCmd}},
-  {MODKEY, XK_p, spawn, {.v = dmenuCmd}},
+  {MODKEY, XK_m, spawn, {.v = dmenuCmd}},
+
   {MODKEY, XK_f, spawn, {.v = firefoxCmd}},
-  {MODKEY|ShiftMask, XK_p, spawn, {.v = passMenuCmd}},
+  {MODKEY, XK_r, spawn, {.v = rocketchatCmd}},
+  {MODKEY, XK_t, spawn, {.v = thunderbirdCmd}},
+
   {MODKEY, XK_Print, spawn, {.v = screenshotCmd}},
   {MODKEY|ControlMask, XK_Print, spawn, {.v = screenshotToClipboardCmd}},
   {MODKEY|ShiftMask, XK_Print, spawn, {.v = selectionScreenshotCmd}},
   {MODKEY|ControlMask|ShiftMask, XK_Print, spawn, {.v = selectionScreenshotToClipboardCmd}},
+
   {MODKEY, XK_Up, spawn, {.v = increaseVolumeCmd}},
   {MODKEY, XK_Down, spawn, {.v = decreaseVolumeCmd}},
   {MODKEY|ShiftMask, XK_Up, spawn, {.v = setMaxVolumeCmd}},
   {MODKEY|ShiftMask, XK_Down, spawn, {.v = muteVolumeCmd}},
+
   {MODKEY, XK_Right, spawn, {.v = increaseBrightnessCmd}},
   {MODKEY, XK_Left, spawn, {.v = decreaseBrightnessCmd}},
   {MODKEY|ShiftMask, XK_Right, spawn, {.v = setMaxBrightnessCmd}},
+
   {MODKEY, XK_b, togglebar, {0}},
   {MODKEY, XK_j, focusstack, {.i = +1}},
   {MODKEY, XK_k, focusstack, {.i = -1}},
